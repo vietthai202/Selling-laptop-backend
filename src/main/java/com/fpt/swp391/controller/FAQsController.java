139097d1af9c377402768873e18d6731e2fac38f
Quang Truong <truongnq2k@gmail.com>
2023-06-13 07:48:15 +0700

    fix: faq

--- src/main/java/com/fpt/swp391/controller/FAQsController.java
+++ src/main/java/com/fpt/swp391/controller/FAQsController.java
@@ -1,9 +1,9 @@
 package com.fpt.swp391.controller;
 
-import com.fpt.swp391.dto.FAPsDto;
+import com.fpt.swp391.dto.FAQsDto;
 import com.fpt.swp391.exceptions.ApiExceptionResponse;
-import com.fpt.swp391.model.FAPs;
-import com.fpt.swp391.service.FAPsService;
+import com.fpt.swp391.model.FAQs;
+import com.fpt.swp391.service.FAQsService;
 import com.fpt.swp391.utils.ApiSuccessResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,17 +13,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/faps")
-public class FAPsController {
-    private final FAPsService faPsService;
+@RequestMapping("/faqs")
+public class FAQsController {
+    private final FAQsService faPsService;
 
-    public FAPsController(FAPsService faPsService) {
+    public FAQsController(FAQsService faPsService) {
         this.faPsService = faPsService;
     }
 
     @PostMapping("/create")
-    public ResponseEntity<?> createFaps(@RequestBody FAPsDto faps) {
-        FAPsDto dto = faPsService.create(faps);
+    public ResponseEntity<?> createFaps(@RequestBody FAQsDto faps) {
+        FAQsDto dto = faPsService.create(faps);
         if (dto != null) {
             return ResponseEntity.status(HttpStatus.OK).body(dto);
         }
@@ -32,7 +32,7 @@ public class FAPsController {
     }
 
     @PostMapping("/create-multiple/{laptopId}")
-    public ResponseEntity<?> createFap(@RequestBody List<FAPsDto> listFaps, @PathVariable Long laptopId) {
+    public ResponseEntity<?> createFap(@RequestBody List<FAQsDto> listFaps, @PathVariable Long laptopId) {
         boolean created = faPsService.createFapsMultiple(listFaps, laptopId);
         if (created) {
             final ApiSuccessResponse response = new ApiSuccessResponse("Successful", HttpStatus.OK, LocalDateTime.now());
@@ -44,7 +44,7 @@ public class FAPsController {
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteFaps(@PathVariable Long id) {
-        FAPs f = faPsService.findById(id);
+        FAQs f = faPsService.findById(id);
         if (f != null) {
             boolean isDelete = faPsService.deleteFapsById(f.getId());
             if (isDelete) {
@@ -58,7 +58,7 @@ public class FAPsController {
 
     @GetMapping("/list")
     public ResponseEntity<?> getAll() {
-        List<FAPsDto> list = faPsService.listAllFaps();
+        List<FAQsDto> list = faPsService.listAllFaps();
         if (list.size() > 0) {
             return ResponseEntity.status(HttpStatus.OK).body(list);
         }
@@ -67,8 +67,8 @@ public class FAPsController {
     }
 
     @PostMapping("/edit/{id}")
-    public ResponseEntity<?> updateFaps(@PathVariable Long id, @RequestBody FAPsDto faps) {
-        FAPs f = faPsService.updateFaps(id, faps);
+    public ResponseEntity<?> updateFaps(@PathVariable Long id, @RequestBody FAQsDto faps) {
+        FAQs f = faPsService.updateFaps(id, faps);
         if (f != null) {
             final ApiSuccessResponse response = new ApiSuccessResponse("Update Successful", HttpStatus.OK, LocalDateTime.now());
             return ResponseEntity.status(HttpStatus.OK).body(response);
