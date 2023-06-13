efd6a43467e8417a023cfeb75dce6c1ba83c5810
Thái <vietthai202a@gmail.com>
2023-06-13 08:11:12 +0700

    feat: update faqs
    Signed-off-by: Thái <vietthai202a@gmail.com>

--- src/main/java/com/fpt/swp391/repository/FAQsRepository.java
+++ src/main/java/com/fpt/swp391/repository/FAQsRepository.java
@@ -4,6 +4,11 @@ import com.fpt.swp391.model.FAQs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FAQsRepository extends JpaRepository<FAQs,Long> {
+
+    List<FAQs> findFAQsByLaptopId(Long laptopId);
+
 }
