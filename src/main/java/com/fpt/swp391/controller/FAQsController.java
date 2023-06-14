bdca8d8a1ecfc0d4ea5d2f945a69b57072978760
Quang Truong <truongnq2k@gmail.com>
2023-06-14 15:44:15 +0700

    fix: faq

--- src/main/java/com/fpt/swp391/controller/FAQsController.java
+++ src/main/java/com/fpt/swp391/controller/FAQsController.java
@@ -15,15 +15,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/faqs")
 public class FAQsController {
-    private final FAQsService faPsService;
+    private final FAQsService faqService;
 
-    public FAQsController(FAQsService faPsService) {
-        this.faPsService = faPsService;
+    public FAQsController(FAQsService faqService) {
+        this.faqService = faqService;
     }
 
     @PostMapping("/create")
-    public ResponseEntity<?> createFaps(@RequestBody FAQsDto faps) {
-        FAQsDto dto = faPsService.create(faps);
+    public ResponseEntity<?> createFaq(@RequestBody FAQsDto faq) {
+        FAQsDto dto = faqService.create(faq);
         if (dto != null) {
             return ResponseEntity.status(HttpStatus.OK).body(dto);
         }
@@ -32,10 +32,10 @@ public class FAQsController {
     }
 
     @PostMapping("/create-multiple/{laptopId}")
-    public ResponseEntity<?> createFap(@RequestBody List<FAQsDto> listFaps, @PathVariable Long laptopId) {
-        boolean created = faPsService.createFapsMultiple(listFaps, laptopId);
+    public ResponseEntity<?> createFaq(@RequestBody List<FAQsDto> listFaq, @PathVariable Long laptopId) {
+        boolean created = faqService.createFapsMultiple(listFaq, laptopId);
         if (created) {
-            final ApiSuccessResponse response = new ApiSuccessResponse("Successful", HttpStatus.OK, LocalDateTime.now());
+            final ApiSuccessResponse response = new ApiSuccessResponse("Create Successful", HttpStatus.OK, LocalDateTime.now());
             return ResponseEntity.status(HttpStatus.OK).body(response);
         }
         final ApiExceptionResponse response = new ApiExceptionResponse("Not Successful", HttpStatus.BAD_REQUEST, LocalDateTime.now());
@@ -43,10 +43,10 @@ public class FAQsController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deleteFaps(@PathVariable Long id) {
-        FAQs f = faPsService.findById(id);
+    public ResponseEntity<?> deleteFaq(@PathVariable Long id) {
+        FAQs f = faqService.findById(id);
         if (f != null) {
-            boolean isDelete = faPsService.deleteFapsById(f.getId());
+            boolean isDelete = faqService.deleteFapsById(f.getId());
             if (isDelete) {
                 final ApiSuccessResponse response = new ApiSuccessResponse("Delete Successful", HttpStatus.OK, LocalDateTime.now());
                 return ResponseEntity.status(HttpStatus.OK).body(response);
@@ -58,7 +58,7 @@ public class FAQsController {
 
     @GetMapping("/list")
     public ResponseEntity<?> getAll() {
-        List<FAQsDto> list = faPsService.listAllFaps();
+        List<FAQsDto> list = faqService.listAllFaps();
         if (list.size() > 0) {
             return ResponseEntity.status(HttpStatus.OK).body(list);
         }
@@ -68,7 +68,7 @@ public class FAQsController {
 
     @GetMapping("/lists/{laptopId}")
     public ResponseEntity<?> getAllByLaptopId(@PathVariable Long laptopId) {
-        List<FAQsDto> list = faPsService.listAllFaqsByLaptopId(laptopId);
+        List<FAQsDto> list = faqService.listAllFaqsByLaptopId(laptopId);
         if (list.size() > 0) {
             return ResponseEntity.status(HttpStatus.OK).body(list);
         }
@@ -77,13 +77,32 @@ public class FAQsController {
     }
 
     @PostMapping("/edit/{id}")
-    public ResponseEntity<?> updateFaps(@PathVariable Long id, @RequestBody FAQsDto faps) {
-        FAQs f = faPsService.updateFaps(id, faps);
+    public ResponseEntity<?> updateFaq(@PathVariable Long id, @RequestBody FAQsDto faq) {
+        FAQs f = faqService.updateFaq(id, faq);
         if (f != null) {
-            final ApiSuccessResponse response = new ApiSuccessResponse("Update Successful", HttpStatus.OK, LocalDateTime.now());
+            final ApiSuccessResponse response = new ApiSuccessResponse("Update FAQ Successful", HttpStatus.OK, LocalDateTime.now());
             return ResponseEntity.status(HttpStatus.OK).body(response);
         }
         final ApiExceptionResponse response = new ApiExceptionResponse("Not Successful", HttpStatus.BAD_REQUEST, LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        FAQs faq = faqService.findById(id);
+        FAQsDto dto = new FAQsDto();
+
+        if(faq != null) {
+            dto.setId(faq.getId());
+            dto.setTitle(faq.getTitle());
+            dto.setContent(faq.getContent());
+            dto.setLaptop_id(faq.getLaptop().getId());
+        }
+
+        if (dto.getId() != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(dto);
+        }
+        final ApiExceptionResponse response = new ApiExceptionResponse("Không có dữ liệu!", HttpStatus.NO_CONTENT, LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
     }
 }
